2c483d5b3ac2b785e9f2d85ea20dde7ef5c66c7d
julied20 <julie.djeffal@gmail.com>
2014-11-20 10:19:52 +0100

    Adapt interface to be more simple

--- ProjetOculusJDFZ/Source/ProjetOculusJDFZ/Interface.h
+++ ProjetOculusJDFZ/Source/ProjetOculusJDFZ/Interface.h
@@ -8,6 +8,8 @@ Class: Interface
 #include "DataFromJson.h"
 #include "Object.h"
 #include<list>
+#include<vector>
+#include<string>
 
 
 
@@ -17,28 +19,35 @@ class Interface {
 
 		// add a face 
 		// a texture is represented by is id (int)
-		virtual Object add_object(std::list<float> position, 
-			std::list<int> textures) = 0;
-		// Remove an object from the Environment
-		virtual void remove_object(std::list<float> position,
-			std::list<int> textures) = 0;
-		virtual Object add_sound_source(std::list<float> position, 
-			std::list<float> directionVector, int sourceId) = 0;
+		virtual void add_face(std::list<float> position, 
+			int texture, int faceId) = 0;
+		
+		// Remove a face from the Environment
+		virtual void remove_face(int faceId) = 0;
+		
+		//Move a face in the Environment
+		virtual void move_face(int faceId) = 0;
+		
+		//Add a sound source to the environment
+		virtual void add_sound_source(std::string name, 
+			std::list<float> position, 
+			std::list<std::vector<float>> viewDirection, 
+			std::list<std::vector<float>> upDirection, int sourceId) = 0;
+		
 		// Remove a sound source from the environment
-		virtual void remove_sound_source(std::list<float> position,
-			int sourceId) = 0;
-		virtual void move_object(std::list<float> position,
-			std::list<int> textures) = 0;
+		virtual void remove_sound_source(int sourceId) = 0;
+		
 		// Move a source sound from the environment
-		virtual void move_source(std::list<float> position,
-			std::list<float> directionVector, int sourceId) = 0;
-		// enable or disable the sound of a source sound
+		virtual void move_source(int sourceId) = 0;
+		
+		// Enable or disable the sound of a source sound
 		virtual void enable_sound(bool enable) = 0;
-		// Set the initial position and view of the receiver
-		// Equal to reset_position()?
-		virtual void set_initial_position() = 0;
-		virtual void set_position(std::list<float> position) = 0;
+		
+		// Set the initial position 
+		virtual void set_initial_position(std::list<float> position) = 0;
 
+		// Reset the position to the initial position
+		virtual void reset_position() = 0;
 
 
 
