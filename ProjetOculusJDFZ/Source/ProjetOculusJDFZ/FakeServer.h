c748c5fc098ff35373485f6254a9c12e62587d57
Fabien Zellweger <fabien.zellweger@epfl.ch>
2015-01-13 16:36:29 +0100

    add empty FakeServer class

---
+++ ProjetOculusJDFZ/Source/ProjetOculusJDFZ/FakeServer.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class PROJETOCULUSJDFZ_API FakeServer
+{
+public:
+	FakeServer();
+	~FakeServer();
+};
