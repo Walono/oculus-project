5c6677cc391ff3b365e5b5d9be12e7a849b756a6
julied20 <julie.djeffal@gmail.com>
2015-01-14 22:02:36 +0100

    Update ParseFromJson.h

--- ProjetOculusJDFZ/Source/ProjetOculusJDFZ/ParseFromJson.h
+++ ProjetOculusJDFZ/Source/ProjetOculusJDFZ/ParseFromJson.h
@@ -1,11 +1,13 @@
-/*
-Class: ParseFromJson
+/**
+*	Bachelor Project: Development and Implementation of an Oculus Rift Frontend for Audio-Visual VR Applications
+*	ParseFromJson
+*	Purpose: It's a Parser which take the JSON message we receive 
+*		from the server and extracts all the data 
+*		to store them in a DataFromJson object.
+*
+*  @author Julie Djeffal & Fabien Zellweger
+**/
 
-It's a Parser which take the JSON message we receive 
-fromthe server and extracts all the data 
-to store them in a DataFromJson object.
-
-*/
 #pragma once
 #include<string>
 #include "DataFromJson.h"
